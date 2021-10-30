61d43e9768037db97b01e3f2d161816cdeae69e3
Tulika Das <tulikadas1029@gmail.com>
2021-10-30 13:40:59 +0530

    slide add and responsive

--- src/js/_homepage.js
+++ src/js/_homepage.js
@@ -9,8 +9,38 @@ if (document.querySelector('.hero-section')) {
         nav: false,
         // controlsContainer: '.hero-section .testimonial-nav',
         autoplay: true,
+        controls:false,
+        autoplayTimeout:50000,
         autoplayButtonOutput: false,
-        controlsText:['<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>']
+        controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            768:{
+              items: 1,
+              controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+              controls:true
+            }
+        }
+    })
+}
+
+if (document.querySelector('.blog-sec')) {
+    tns({
+        container: '.blog-sec .slides',
+        items: 1,
+        nav: false,
+        // controlsContainer: '.hero-section .testimonial-nav',
+        autoplay: true,
+        controls:false,
+        autoplayTimeout:50000,
+        autoplayButtonOutput: false,
+        controlsText:['<i class="fas fa-chevron-left"></i>' ,'<i class="fas fa-chevron-right"></i>'],
+        responsive: {
+            768:{
+              items: 1,
+              controlsText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+              controls:true
+            }
+        }
     })
 }
 
